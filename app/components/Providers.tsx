42d2ecf3224448548432888bf229d03482c5e91f
PersianDavinci <persiandavinci@gmail.com>
2023-11-05 00:26:26 +0330

    add domain to providers

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://cbc-minimal-shop.vercel.app/stripe/success"
+      cancelUrl="https://cbc-minimal-shop.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
